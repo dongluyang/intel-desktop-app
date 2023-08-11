8c1eb425cfc2d02e2675ac34ea5842f850daff68
dongluyang <dongluyang2005@126.com>
2023-08-11 22:44:53 +0800

    add login

---
+++ src/renderer/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-form :model="form" :style="{width:'600px'}">
+    <a-form-item field="userName" label="用户名" validate-trigger="input" required>
+      <a-input v-model="form.userName" placeholder="请输入用户名" />
+    </a-form-item>
+    <a-form-item field="password" label="密码" validate-trigger="input" required>
+      <a-input-password v-model="form.password" placeholder="请输入密码" />
+    </a-form-item>
+  </a-form>
+<!--  {{ form }}-->
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const form = reactive({
+      userName: '',
+      password: '',
+    })
+
+    return {
+      form,
+    }
+  },
+}
+</script>
+
--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -1,15 +1,25 @@
 <script setup>
   import { useRouter, useRoute } from 'vue-router';
+  import { ref } from "vue";
+  import Login from '../components/Login.vue'
   const router = useRouter();
   const route = useRoute();
   const handleClick2 = () => {
         router.replace({path:'/about'});
    }
+  const showLogin = ref(false);
+  const handleOk = () => {
+    showLogin.value = true;
+  };
+  const handleCancel = () => {
+    showLogin.value = false;
+  }
 </script>
 
 <template>
 
  <div>
+   <!-- 在这里添加按钮并进行右对齐 -->
   <a-tabs>
 
 
@@ -29,41 +39,25 @@
       default-collapsed="false"
       @collapse="onCollapse"
     >
-      <a-sub-menu key="0">
-        <template #icon><icon-apps></icon-apps></template>
-        <template #title>Navigation 1</template>
-        <a-menu-item key="0_0">Menu 1</a-menu-item>
-        <a-menu-item key="0_1">Menu 2</a-menu-item>
-        <a-menu-item key="0_2">Menu 3</a-menu-item>
-        <a-menu-item key="0_3">Menu 4</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="1">
-        <template #icon><icon-bug></icon-bug></template>
-        <template #title>Navigation 2</template>
-        <a-menu-item key="1_0">Menu 1</a-menu-item>
-        <a-menu-item key="1_1">Menu 2</a-menu-item>
-        <a-menu-item key="1_2">Menu 3</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="2">
-        <template #icon><icon-bulb></icon-bulb></template>
-        <template #title>Navigation 3</template>
-        <a-menu-item key="2_0">Menu 1</a-menu-item>
-        <a-menu-item key="2_1">Menu 2</a-menu-item>
-        <a-sub-menu key="2_2" title="Navigation 4">
-          <a-menu-item key="2_2_0">Menu 1</a-menu-item>
-          <a-menu-item key="2_2_1">Menu 2</a-menu-item>
-        </a-sub-menu>
-      </a-sub-menu>
+      <a-menu-item key="0">
+        <a-button type="text">
+          <template #icon>
+            <icon-pen-fill />
+          </template>
+          <template #default>待我提交</template>
+        </a-button>
+      </a-menu-item>
+      <a-menu-item key="1">
+        <a-button type="text">
+          <template #icon>
+            <icon-tool />
+          </template>
+          <template #default>待我审验</template>
+        </a-button>
+      </a-menu-item>
     </a-menu>
   </div>
     </a-tab-pane>
-
-
-
-
-
-
-
 
 
 
@@ -168,10 +162,33 @@
        <p>{{ $t('welcome') }}</p>
     </a-tab-pane>
   </a-tabs>
-  </div>
+   <a-button type="primary" class="floating-button larger-button" size="medium" @click="handleOk">登录</a-button>
+   <a-modal v-model:visible="showLogin" @ok="handleOk" @cancel="handleCancel">
+     <template #title>
+       登录系统
+     </template>
+     <Login></Login>
+   </a-modal>
+ </div>
 </template>
 
 <style scoped>
+/* 使用 Flexbox 布局将按钮和 a-tabs 放在同一行并右对齐 */
+.tab-container {
+  display: flex;
+  justify-content: space-between;
+  /*align-items: center;*/
+}
+.floating-button {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+/* 自定义更大的按钮样式 */
+.larger-button {
+  font-size: 20px; /* 根据需要调整字体大小 */
+  padding: 16px 24px; /* 根据需要调整内边距 */
+}
 .menu-demo {
   box-sizing: border-box;
   width: 100%;
